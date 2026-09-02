c4917f66caddd290221991c449319feca83883ba
Tharika Akurana <tharikaakurana1998@gmail.com>
2026-09-02 08:53:43 +0530

    correctly added PyPI badge

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-0bnikeax@p9i^0r9uf@u8xc9vbbqgv^m9%@pzw!rjq8a03(kz-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['automr-web-interface.onrender.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['automr-web-interface.onrender.com']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
